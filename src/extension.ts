ce5cd40628ed1809ba86f4b6c04fae8e25633764
Paulo Aboim Pinto <aboimpinto@gmail.com>
2022-01-27 01:14:19 +0100

    Get the LanguageServer in dynamic way. Still not getting from libs folder. Need to work on that

--- src/extension.ts
+++ src/extension.ts
@@ -15,6 +15,7 @@ import {
     createServerPipeTransport,
 } from "vscode-languageclient/node";
 import { AvaloniaNewFileManager } from './AvaloniaNewFileManager';
+import path from 'path';
 
 let client: LanguageClient;
 
@@ -26,15 +27,34 @@ export function activate(context: vscode.ExtensionContext)
 	const newFileManagement = new AvaloniaNewFileManager();
 	newFileManagement.register(context);
 
+	var extension = vscode.extensions.getExtension("PauloAboimPinto.avalonia.vscode.extension");
+
+	const dllPath = 
+		vscode.extensions.getExtension("PauloAboimPinto.avalonia.vscode.extension").extensionPath + 
+		path.sep + 
+		"Avalonia.AXAML.LanguageServer" +
+		path.sep + 
+		"Avalonia.AXAML.LanguageServer" +
+		path.sep + 
+		"bin" +
+		path.sep + 
+		"Debug" +
+		path.sep + 
+		"net6.0" +
+		path.sep + 
+		"Avalonia.AXAML.LanguageServer.dll";
+
+	console.log("--> " + dllPath);
+
 	let serverOptions: ServerOptions = {
 		run: {
 			command: "dotnet",
-			args: ["/home/esqueleto/myWork/Avalonia.VSCodeExtension/Avalonia.AXAML.LanguageServer/Avalonia.AXAML.LanguageServer/bin/Debug/net6.0/Avalonia.AXAML.LanguageServer.dll"],
+			args: [dllPath],
 			transport: TransportKind.pipe
 		},
 		debug: {
 			command: "dotnet",
-			args: ["/home/esqueleto/myWork/Avalonia.VSCodeExtension/Avalonia.AXAML.LanguageServer/Avalonia.AXAML.LanguageServer/bin/Debug/net6.0/Avalonia.AXAML.LanguageServer.dll"],
+			args: [dllPath],
 			transport: TransportKind.pipe,
 			runtime: ""
 		}
